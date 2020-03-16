7e8191ce18e446f127b4eb76761c573033cd48cf
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-03-16 06:55:20 +0100

    Make it compile

--- Lenses.hs
+++ Lenses.hs
@@ -1,8 +1,10 @@
+module Lenses where
 import Control.Lens
 import Data.Monoid
-foldMapOf (traverse . both . _Just) Sum [(Just $ toInteger 21, Just $ toInteger 21)]
 
-foldMapOf (folded . both . _Just) Sum [(Just 21, Just 21)]
+f1=foldMapOf (traverse . both . _Just) Sum [(Just $ toInteger 21, Just $ toInteger 21)]
 
-foldMapOf (traverse . both . _Just) Product [(Just $ toInteger 21, Just $ toInteger 21)]
+f2=foldMapOf (folded . both . _Just) Sum [(Just 21, Just 21)]
+
+f3=foldMapOf (traverse . both . _Just) Product [(Just $ toInteger 21, Just $ toInteger 21)]
 
